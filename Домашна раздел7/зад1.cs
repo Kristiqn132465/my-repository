d3864074a645d6a96db6fe796222b25b4aa90d7d
Kristiqn <kristian.iotov@gmail.com>
2024-11-11 18:13:47 +0200

    Add files via upload

---
+++ Домашна раздел7/зад1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Въведете височината на триъгълника: ");
+        int h = int.Parse(Console.ReadLine());
+
+        Console.Write("Въведете посоката на триъгълника (left или right): ");
+        string pos = Console.ReadLine().ToLower();
+
+        if (pos == "right")
+        {
+            DrawRightAlignedTriangle(h);
+        }
+        else if (pos == "left")
+        {
+            DrawLeftAlignedTriangle(h);
+        }
+        else
+        {
+            Console.WriteLine("Невалидна посока. Моля, въведете 'left' или 'right'.");
+        }
+    }
+
+    static void DrawRightAlignedTriangle(int h)
+    {
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < h - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k <= i; k++)
+            {
+                if (i == h - 1 || k == 0 || k == i)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void DrawLeftAlignedTriangle(int h)
+    {
+        for (int i = 0; i < h; i++)
+        {
+            for (int k = 0; k <= i; k++)
+            {
+                if (i == h - 1 || k == 0 || k == i)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Домашна раздел7/зад2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int countLikes = 0;
+        // the first two names
+        Console.WriteLine("Enter a name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"{firstName} liked your post");
+        Console.WriteLine("Enter a name: ");
+        string secondName = Console.ReadLine();
+        Console.WriteLine($"{firstName} and {secondName} liked your post");
+
+        while (true)
+        {
+            Console.WriteLine("Enter a name: ");
+            string user = Console.ReadLine();
+            if (user == "")
+            {
+                break;
+            }
+            countLikes++;
+            Console.WriteLine($"{firstName}, {secondName} and {countLikes} liked your post");
+        }
+    }
+}
